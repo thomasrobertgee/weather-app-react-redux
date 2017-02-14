038b2f6a9c84ed95898304c5f83096022db359be
Thomas Gee <thomasrobertgee@gmail.com>
2017-02-14 21:33:25 +1100

    mid-tutorial, got the data and setup the container, now adding charts with Sparkline

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,17 @@
-import axios from 'axious'
+import axios from 'axios';
 
-const API_KEY = 'e9136ee417836113576555d7a9a590c4'
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?qappid=${API_KEY}`
+const API_KEY = '3029d7747f52537b52be60401e74cfd0'
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
-// extract to a seperate var and export it
-export const FETCH_WEATHER = 'FETCH_WEATHER'
+// extract to a separate var and export it
+export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},aus`
-  const request = axious.get(url)
+  const request = axios.get(url)
 
   return {
-    type: FETCH_WEATHER // the action, import to reducer (passing through middleware)
+    type: FETCH_WEATHER, // the action, import to reducer (passing through middleware)
     payload: request
-  }
+  };
 }
