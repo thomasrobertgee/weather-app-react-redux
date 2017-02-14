db5cfb062f76afe255458d42d57776a50efad2d5
Thomas Gee <thomasrobertgee@gmail.com>
2017-02-14 23:05:52 +1100

    finished tutorial, looks great

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -29,7 +29,7 @@ class SearchBar extends Component {
     return (
       <form onSubmit={this.onFormSubmit} className="input-group">
         <input
-        placeholder="Get a five-day forecast in your favourite cities"
+        placeholder="e.g. Melbourne"
         className="form-control"
         value={this.state.term}
         onChange={this.onInputChange}
